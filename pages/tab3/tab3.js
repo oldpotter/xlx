c9bca131953a0e895bb60f7b06413437bfa4335c
oldpotter <oldpottertom@icloud.com>
2017-12-07 12:47:18 +0800

    音频答疑

--- pages/tab3/tab3.js
+++ pages/tab3/tab3.js
@@ -1,20 +1,26 @@
 const config = require('../../config.js')
 Page({
 	data: {
-		list: ['修行平台、公众号', '乱炖']
+		list: ['修行平台、公众号', '乱炖', '元吾氏答疑音频']
 	},
 
 	onTap(event) {
 		const index = event.currentTarget.id
+		// console.log(`index:${index}`)
 		if (index == 0) {
 			const articleId = 'MrYr2u'
 			wx.navigateTo({
 				url: `../article/article?articleId=${articleId}`,
 			})
-		} else {
+		} else if (index == 1) {
 			const url = config.service.getSubjectUrl
 			wx.navigateTo({
 				url: `../article-list/article-list?url=${url}&subjectid=${config.subjectid.ld}`,
+			})
+		} else if (index == 2) {
+			const articleId = 'NzqU7b'
+			wx.navigateTo({
+				url: `../article/article?articleId=${articleId}`,
 			})
 		}
 	},
