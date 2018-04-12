7c373a8b0615165f5b1cf947a145b3110abd6970
oldpotter <oldpottertom@icloud.com>
2018-04-12 13:07:42 +0800

    优化

--- client/app.js
+++ client/app.js
@@ -5,6 +5,33 @@ App({
 	version: `v1.7.3`,
 
 	onLaunch() {
+		if (wx.canIUse('getUpdateManager')) {
+			// wx.getUpdateManager 在 1.9.90 才可用，请注意兼容
+			const updateManager = wx.getUpdateManager()
+
+			updateManager.onCheckForUpdate(function (res) {
+				// 请求完新版本信息的回调
+				console.log(res.hasUpdate)
+			})
+
+			updateManager.onUpdateReady(function () {
+				wx.showModal({
+					title: '更新提示',
+					content: '新版本已经准备好，是否马上重启小程序？',
+					success: function (res) {
+						if (res.confirm) {
+							// 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+							updateManager.applyUpdate()
+						}
+					}
+				})
+			})
+
+			updateManager.onUpdateFailed(function () {
+				// 新的版本下载失败
+			})
+		}
+
 		moment.locale('zh', {
 			relativeTime: {
 				past: "%s前",
@@ -53,8 +80,8 @@ App({
 	article: null,//显示的文章
 	collections: null,//收藏的文章ids
 	level: null,//字体大小标示值
-	usingRing:undefined,
+	usingRing: undefined,
 	screenWidth: undefined,
 	screenHeight: undefined,
-	
+
 })
