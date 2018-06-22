132061a1df71d51385703a934ebea9e2af7994a8
oldpotter <oldpottertom@icloud.com>
2018-06-22 15:42:41 +0800

    收藏列表bug

--- client/pages/tab4/collectionlist.js
+++ client/pages/tab4/collectionlist.js
@@ -2,44 +2,45 @@ const app = getApp()
 const config = require('../../config.js')
 const util = require('../../utils/util.js')
 Page({
-	data: {
-		list: [],
-		noAttachment: true,
-	},
+  data: {
+    list: [],
+    noAttachment: true,
+  },
 
-	onLoad() {
-		const _this = this
-		app.collections.forEach(id => {
-			wx.showLoading({
-				title: '请稍后...',
-			})
-			wx.request({
-				url: `${config.service.getArticleUrl}?id=${id}&format=JSON`,
-				success: function (res) {
-					// console.log('收到数据：',res.data)
-					// _this.data.list.push(res.data)
+  onLoad() {
+    // console.log(`app.colliections:${app.collections}`)
+
+    const _this = this
+    
+    app.collections.forEach(id => {
+      wx.showLoading({
+        title: '请稍后...',
+      })
+      wx.request({
+        url: `${config.service.getArticleUrl}?id=${id}&format=JSON`,
+        success: function(res) {
 					let list = _this.data.list
-					list.push(res.data)
-					_this.setData({
-						list: list
-					})
-					// console.log('list:', _this.data.list)
-					wx.hideLoading()
-				},
-				fail: function (res) {
-					console.error('请求失败。。。')
-				},
-			})
-		})
+          list.push(res.data)
+          _this.setData({
+            list
+          })
+					// console.log(list)
+          wx.hideLoading()
+        },
+        fail: function(res) {
+          console.error('请求失败。。。')
+        },
+      })
+    })
+		
+  },
 
-	},
-
-	onClickArticleItem(event) {
-		const articleId = app.collections[event.currentTarget.dataset.index]
-		console.log('click:',articleId)
-		wx.navigateTo({
-			url: `../article/article?articleId=${articleId}`,
-		})
-	},
+  onClickArticleItem(event) {
+		const index = event.currentTarget.dataset.index
+		const articleId = this.data.list[index].id
+    wx.navigateTo({
+      url: `../article/article?articleId=${articleId}`,
+    })
+  },
 
 })
