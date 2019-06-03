30bc0b966753eb9d0eba2c69ce1ac0592e4e3827
oldpotter <oldpottertom@icloud.com>
2019-06-03 10:22:57 +0800

    1.9.3

--- client/pages/tab3/tab3.js
+++ client/pages/tab3/tab3.js
@@ -6,33 +6,34 @@ Page({
 	},
 
 	onTap(event) {
-		const index = event.currentTarget.id
+		// console.log(event.currentTarget.dataset.text)
+		const text = event.currentTarget.dataset.text
 		// console.log(`index:${index}`)
-		if (index == 0) {
+		if (text == '相关平台、公众号') {
 			const articleId = 'MrYr2u'
-			wx.navigateTo({
+			wx.navigateTo({//相关平台
 				url: `../article/article?articleId=${articleId}`,
 			})
-		} else if (index == 1) {
+		} else if (text == '') {
 			const url = config.service.getSubjectUrl
 			wx.navigateTo({
 				url: `../article-list/article-list?url=${url}&subjectid=${config.subjectid.ld}`,
 			})
-		} else if (index == 2) {
+		} else if (text == '元吾氏答疑音频') {
 			const articleId = 'NzqU7b'
-			wx.navigateTo({
+			wx.navigateTo({//音频
 				url: `../article/article?articleId=${articleId}`,
 			})
-		}else if(index==3){
+		} else if (text =='元吾氏集录'){
 			wx.navigateTo({
-				url: '../tab1/tab1',
+				url: '../tab1/tab1',//集录
 				success: function(res) {},
 				fail: function(res) {},
 				complete: function(res) {},
 			})
-		}else if(index ==4){
+		} else if (text =='元吾氏答疑'){
 			wx.navigateTo({
-				url: '../tab2/tab2',
+				url: '../tab2/tab2',//答疑
 				success: function(res) {},
 				fail: function(res) {},
 				complete: function(res) {},
